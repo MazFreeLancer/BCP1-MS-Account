725fb4b17cd9957d392700ee638532b7043fcbbd
MZena <manuelzenareque@gmail.com>
2022-03-03 23:12:18 -0500

    update config server configuration

--- src/main/java/com/nttdata/bcp1/msaccout/config/ProjectConfig.java
+++ src/main/java/com/nttdata/bcp1/msaccout/config/ProjectConfig.java
@@ -1,8 +1,12 @@
 package com.nttdata.bcp1.msaccout.config;
 
+import lombok.Data;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@Data
+@RefreshScope
 @Configuration
 @EnableWebFlux
 public class ProjectConfig {
